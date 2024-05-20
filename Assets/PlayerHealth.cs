8a3105b9022b711bf7d8c1442dd1010883c71cfa
Mathys Cornuaud <mathys.cornuaud85@gmail.com>
2024-05-20 13:24:55 +0200

    Player and enemy
    Start of player health
    Start of enemy behaviour

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealh : MonoBehaviour
+{
+    public int hp;
+    public PlayerMovement movement;
+    public PlayerAttack attack;
+    public Animator anim;
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            hp -= 1;
+        }
+    }
+
+    public void Update()
+    {
+        if (hp <= 0)
+        {
+            hp = 0;
+            movement.enabled = false;
+            attack.enabled = false;
+            anim.SetBool("isDead", true);
+        }
+    }
+    public void GainHealth()
+    {
+        hp += 1;
+    }
+}
